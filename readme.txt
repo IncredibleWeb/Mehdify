84753402a365e7ecd039d241702615e5b3e80dc7
alessandrov <abh_work@outlook.com>
2017-03-10 20:49:05 +0100

    test task

---
+++ readme.txt
@@ -0,0 +1,32 @@
+Hi guys,
+
+a brief overview of what I've done.
+
+I have put my code in main.js as per instructions and run tests accordingly, making the test fil pointing to main.js.
+
+I don't like to reinvent the wheel so I browsed for an efficient, robust version of the needed algorythm and this is the one I adopted:
+
+0:
+Transform the given number into an array of digits
+
+1
+Traverse the array from the rightmost digit. Keep traversing till you get a digit which is smaller than the previously traversed digit:
+let's call it array[x]
+
+2:
+Consider the subarray made of all the digits right side of x. Find the smallest digit (bigger than array[x]) among them 
+(it doesn't matter if there's more than one, pick the first one): let's call it array[y].
+
+3
+Swap array[x] and array[y] 
+
+4
+Sort all the digits right side of array[x]
+
+5
+Re-transform array[] into a number
+
+I think this is an efficient algorythm although I am a bit rusty with Javascript, particularly with the local/global variables handling.
+
+I added a couple of straightforward test lines myself and changed the indentation to 2.
+
--- src/main.js
+++ src/main.js
@@ -1,13 +1,94 @@
 (function() {
 
-    function mehdify(x) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
-    }
-    
-    // required to export for tests
-    // DO NOT TOUCH
-    var exports = module.exports;
-    module.exports = mehdify;
+  function mehdify(number) {
+		// Add your code here
+		console.log("START");
+		
+		let result = number;
+
+		let arrayRepresentationOfGivenNumber = number.toString().split('').map(Number); 
+		
+		let digits = number.toString().split('');
+		console.log('digits....' + arrayRepresentationOfGivenNumber);
+
+		for (i = arrayRepresentationOfGivenNumber.length -1; i >= 0; i--) {
+			
+			if (arrayRepresentationOfGivenNumber[i] > arrayRepresentationOfGivenNumber[i-1]){
+				
+				let firstIndex = i-1;
+				console.log('index of the first element to be swapped, x : ' + firstIndex);
+							
+				let secondIndex = findSecondIndex(arrayRepresentationOfGivenNumber);
+				console.log('index of the second element to be swapped, y : ' + secondIndex);
+
+				//swap elements
+				let	swappedElementsArray = swapElements(arrayRepresentationOfGivenNumber, firstIndex, secondIndex);
+				console.log('array with swapped elements, yet to be sorted: ' + swappedElementsArray);
+
+				//sort the subarray starting at swappedElementsArray[x]
+				resultArray = sortSubArrayOf(arrayRepresentationOfGivenNumber, swappedElementsArray, firstIndex);
+					
+				//transform the array back into a number
+				result = resultArray.join('');
+																			
+				break;
+			}
+		}
+		
+		console.log('result.... ' + result);		
+		return result;
+  }
+	
+	function sortSubArrayOf(originalArray, arrayToBeSorted, firstIndex){		
+		let subarray = arrayToBeSorted.slice(firstIndex + 1); 
+
+		let subarraySorted = subarray.sort(function(a,b){return a - b})
+
+		let firstArray = originalArray.slice(0, firstIndex + 1);
+		
+		let result = firstArray.concat(subarraySorted);
+		
+		return result;
+	}
+	
+	function findSecondIndex(originalArray){
+		let result = -1;
+		
+		let subarray = originalArray.slice(i); 
+		console.log('subarray :' + subarray);
+								
+		let secondOne = findTheIndexOfTheSmallestIntegerBiggerThanATreshold(originalArray[i-1], subarray);
+		console.log('secondOne.... '+secondOne);
+
+		result = subarray.indexOf(secondOne) + (originalArray.length - subarray.length);
+		console.log('secondIndex.... ' + result);
+		
+		return result;
+	}
+	
+	function swapElements(intArray, firstIndex, secondIndex){
+		let buffer = intArray[firstIndex];
+		
+		intArray[firstIndex] = intArray[secondIndex];
+		intArray[secondIndex] = buffer;
+				
+		return intArray;
+	}
+	
+	function findTheIndexOfTheSmallestIntegerBiggerThanATreshold(treshold, intArray){
+		result = 10;
+		
+		for (i = 0; i < intArray.length; i++) {
+			if (intArray[i] < result && intArray[i] > treshold){
+				result = intArray[i];
+			}
+		}
+		
+		return result;
+	}
+	    
+  // required to export for tests
+  // DO NOT TOUCH
+  var exports = module.exports;
+  module.exports = mehdify;
 })();
