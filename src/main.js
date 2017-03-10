73c5c34c0433c9d294bc8e577d7d5b53bc8ddbea
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-03-10 09:02:50 +0100

    add jsdoc and helper validation function

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
 (function() {
 
+    /**
+     * Calculate number that is bigger or equals for argument `x`.
+     * @param {number} x - The input number.
+     * @return {number}
+     */
     function mehdify(x) {
-        // Add your code here
-        console.log("Hello world!");
+        if (!isInputValid(x)) {
+            return -1;
+        }
+
         return -1;
+    }
+    
+    /**
+     * Validate input number. Check if number belongs to positive save integer range.
+     * @param {number} number - The number to check.
+     * @return {boolean} `true` if number is ok and `false` f number is not valid.
+     */
+    function isInputValid(number) {
+        return (Number.isFinite(number)) && (Number.isInteger(number)) && (number > 0) && (number < Number.MAX_SAFE_INTEGER);
     }
     
     // required to export for tests
