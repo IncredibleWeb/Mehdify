956b34ef3f86a539f0ca408251ee31d5253ecc04
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-03-15 04:44:20 +0100

    ad modified files

--- test/test.js
+++ test/test.js
@@ -1,14 +1,14 @@
 var assert = require('assert');
 var mehdify = require('../dist/script.min.js');
 
-describe('mehdify', function() {
-    it('should return the next biggest integer using the same digits', function() {
+describe('mehdify', function () {
+    it('should return the next biggest integer using the same digits', function () {
         assert.equal(32214, mehdify(32142));
         assert.equal(491, mehdify(419));
         assert.equal(712, mehdify(271));
         assert.equal(81, mehdify(18));
     });
-    it('should return X when there is no larger integer', function() {
+    it('should return X when there is no larger integer', function () {
         assert.equal(542, mehdify(542));
         assert.equal(321, mehdify(321));
         assert.equal(9100, mehdify(9100));
