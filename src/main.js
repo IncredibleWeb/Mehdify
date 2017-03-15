b440cf16ef91fe6c92184cc152b62479c9ab8bce
Vasyl Melnychuk <vmmelnychuk@gmail.com>
2017-03-15 02:05:54 +0100

    changer gitignore

--- src/main.js
+++ src/main.js
@@ -1,21 +1,13 @@
 (function () {
-
-    /**
-     * Calculate number that is bigger or equals for argument `x`.
-     * @param {number} x - The input number.
-     * @return {number}
-     * * -1 if number is not valid or return number that is equarls or greater `x`
-     * 
-     * return shit back if case of bad requesrs or just 0
-     */
     function mehdify(x) {
-        if (inputIsNotValid(x)) return -1;
-
+        if (inputIsNotValid(x)) {
+            return -1;
+        }
         const answer = findAnswer(x);
         return answer;
     }
 
-    /* Main login function */
+    /* Main logic function */
     function findAnswer(x) {
         const digits = numberToDigits(x);
         if (digitsIsInDescendingOrder(digits)) {
@@ -23,33 +15,18 @@
         }
 
         if (digitsIsInAscendingOrder(digits)) {
-            return swapLastTwoDigits(digits);
+            return digitsToNumber(swapLastTwoDigits(digits));
         }
 
         return solveGeneralCase(digits);
     }
 
     /* Core logic function */
-
-    function digitsIsInDescendingOrder(digits) {
-        const len = digits.length;
-        if (len === 0) {
-            return false;
-        }
-        for (var i = len - 1; i > 0; i--) {
-            if (digits[i] > digits[i - 1]) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-
     function solveGeneralCase(digits) {
         const len = digits.length;
         const last = digits[len - 1];
         let first = -1;
-        for (var i = len - 1; i > 0; i--) {
+        for (let i = len - 1; i > 0; i--) {
             if (digits[i] < digits[last]) {
                 first = i;
                 break;
@@ -65,7 +42,20 @@
         return answer;
     }
 
-    /* Helper functions */
+    /* Helper functions: start */
+    function digitsIsInDescendingOrder(digits) {
+        const len = digits.length;
+        if (len === 0) {
+            return false;
+        }
+        for (let i = len - 1; i > 0; i--) {
+            if (digits[i] > digits[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     function numberIsPositiveSafeInteger(number) {
         return (Number.isFinite(number)) &&
             (Number.isInteger(number)) &&
@@ -124,7 +114,7 @@
         if (len === 0) {
             return false;
         }
-        for (var i = len - 1; i > 0; i--) {
+        for (let i = len - 1; i > 0; i--) {
             if (digits[i] > digits[i - 1]) {
                 return false;
             }
@@ -143,34 +133,15 @@
 
     function digitsIsInAscendingOrder(digits) {
         const len = digits.length - 1;
-        for (var i = 0; i < len - 1; i++) {
+        for (let i = 0; i < len - 1; i++) {
             if (digits[i] > digits[i + 1]) {
                 return false;
             }
         }
         return true;
     }
+    /* Helper functions: end */
 
-
-    function getNextFromAscendingOrderNumber(digits) {
-        return -1;
-    }
-
-
-
-
-    /**
-     * How it works
-     * 2. algorithm
-     * 3. validate answer/output
-     * 
-     * 2. algorithm
-     * 2.1 check if it decendent order
-     * 2.2 check if in indendent order
-     * 2.3 modify number
-     * 
-     * use array if digits
-     */
 
     // required to export for tests
     // DO NOT TOUCH
