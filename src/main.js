57ac3b7b80d1a33c15a7bd708ff649e98303ea6f
Nicoleza <nicole.orpen@gmail.com>
2018-02-23 14:37:09 +0100

    Corrected mehdify function

--- src/main.js
+++ src/main.js
@@ -1,24 +1,17 @@
 (function() {
 
     function mehdify(x) {
-        var permutations = [];
-        var permute = function(arr, m) {
-            if (m === void 0) { m = []; }
-            if (arr.length === 0) {
-                permutations.push(parseInt(m.join("")));
-            } else {
-                for (var i = 0; i < arr.length; i++) {
-                    var curr = arr.slice();
-                    var next = curr.splice(i, 1);
-                    permute(curr.slice(), m.concat(next));
-                }
+        var arr = x.toString().split("");
+        for (i = arr.length - 1; i >= 0; i--) {
+            for (j = i - 1; j >= 0; j--) {
+                var a = arr.slice(0, j).join("");
+                var b = arr[i];
+                var c = arr.slice(j, i).concat(arr.slice(i + 1)).sort().join("");
+                var result = parseInt(a + b + c);
+                if (result > x) return result;
             }
-        };
-        //generate permutations
-        permute(x.toString().split(""));
-
-        //get next highest value
-        return permutations.sort().reduce(function(acc, val) { return acc <= x ? val : acc; });
+        }
+        return x;
     }
 
     // required to export for tests
