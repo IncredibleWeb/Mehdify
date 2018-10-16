8b59cdf5a349e9be1506eeba987419f200a28199
KHEDER KASEM <khader136@hotmail.com>
2018-10-16 11:49:19 +0300

    Code Added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,45 @@
 (function() {
-
+	
     function mehdify(x) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
+
+	    var numArray = x.toString().split('');  
+		var s = -1;   
+	
+		  for (var i = numArray.length-1; i > 0; i--) {
+			if (+numArray[i] > +numArray[i-1]) {
+			  s = i-1;
+			  break;
+			}
+		  }
+  
+		  if (s == -1) 
+			  return x;    
+    
+		  var cahngePart = numArray.splice(s);   
+		  var pivot = cahngePart.splice(0, 1)[0]; 
+		  
+		  var efNumber = null, efNumberindex = null;
+		  for (var i = 0; i < cahngePart.length; i++) {
+			if (cahngePart[i] > pivot) {
+			  if (efNumber == null || cahngePart[i] < efNumber) {
+				efNumber = cahngePart[i];
+				efNumberindex = i;
+			  }
+			}
+		  }
+
+		  if (efNumberindex == null) 
+			  return x;
+		  
+		  cahngePart.splice(efNumberindex, 1);
+		  cahngePart.push(pivot);
+		  cahngePart = cahngePart.sort();
+		  
+		  var result = +numArray.concat([efNumber]).concat(cahngePart).join('');
+		  if (result < x) 
+			  return x;
+		  
+		return result;
     }
     
     // required to export for tests
