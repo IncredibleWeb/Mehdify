19b6724af27eae73f5178ef84bdbc2273b8c7ceb
NicMul <34182208+NicMul@users.noreply.github.com>
2018-11-28 06:09:58 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 (function() {
+    
+        var c = x;
+		var d = x.toString().split('').map(function (x) { return parseInt(x);}).sort();
+		var m = parseInt(d.reverse().join('')) + 1;
 
-    function mehdify(x) {
-        // Add your code here
-        console.log("Hello world!");
-        return -1;
-    }
+		while (true) {
+			c++;
+			var i = c.toString().split('').map(Number).sort().reverse();
+			if (d.join('') === i.join('')) {
+				return c;
+			} else if ( c === m ) {
+				return x;
+			}
+		}
     
     // required to export for tests
     // DO NOT TOUCH
