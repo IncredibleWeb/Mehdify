ea69e43128fc5c30c48e7bbeedc3fb006227bb5a
NicMul <34182208+NicMul@users.noreply.github.com>
2018-11-28 06:38:52 +0100

    Add function header and code to solve algorithm
    nicholis muller interview test

--- src/main.js
+++ src/main.js
@@ -1,19 +1,22 @@
-(function() {
-    
-        var c = x;
-		var d = x.toString().split('').map(function (x) { return parseInt(x);}).sort();
-		var m = parseInt(d.reverse().join('')) + 1;
+(function () {
 
-		while (true) {
-			c++;
-			var i = c.toString().split('').map(Number).sort().reverse();
-			if (d.join('') === i.join('')) {
-				return c;
-			} else if ( c === m ) {
-				return x;
-			}
+    function mehdify(x) {
+  
+        var c = x;
+	var d = x.toString().split('').map(function (x) { return parseInt(x);}).sort();
+	var m = parseInt(d.reverse().join('')) + 1;
+
+	while (true) {
+		c++;
+		var i = c.toString().split('').map(Number).sort().reverse();
+		if (d.join('') === i.join('')) {
+			return c;
+		} else if (c === m) {
+			return x;
 		}
-    
+	}
+    }
+
     // required to export for tests
     // DO NOT TOUCH
     var exports = module.exports;
